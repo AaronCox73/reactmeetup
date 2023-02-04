28cfb056abddfa1c70d4eccc34a94ac9379fdabc
AJ Cox <ajcox@ajs-macbook-pro.lan>
2023-02-03 20:51:54 -0700

    now done for real

--- src/components/layout/MainNav.js
+++ src/components/layout/MainNav.js
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import cssClass from "./MainNav.module.css";
+import FavoritesContext from "../../store/favorites-context";
 
 function MainNav() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <header className={cssClass.header}>
       <div className={cssClass.logo}>React Meetups</div>
@@ -15,7 +19,12 @@ function MainNav() {
             <Link to="/new">Add New Meetups</Link>
           </li>
           <li>
-            <Link to="/favorites">My Favorites</Link>
+            <Link to="/favorites">
+              My Favorites
+              <span className={cssClass.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
           </li>
         </ul>
       </nav>
